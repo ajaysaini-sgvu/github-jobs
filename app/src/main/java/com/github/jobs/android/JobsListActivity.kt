a49c5161f2f2b9b25e81cccdc6e855191c48fc47
Ajay <engineer.ajaysaini@gmail.com>
2018-05-20 16:32:36 +0530

    Setup project structure :blush:

---
+++ app/src/main/java/com/github/jobs/android/JobsListActivity.kt
@@ -0,0 +1,12 @@
+package com.github.jobs.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class JobsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jobs_list)
+    }
+}
